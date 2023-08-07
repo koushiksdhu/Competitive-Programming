df270705dbd3621708dc3a4df3cea0132256bfd7
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-08-07 18:31:44 +0530

    Added solution - LeetHub

---
+++ Find the smallest and second smallest element in an array - GFG/find-the-smallest-and-second-smallest-element-in-an-array.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            long[] product = obj.minAnd2ndMin(a, n); 
+            if(product[0]==-1)
+                System.out.println(product[0]);
+            else
+                System.out.println(product[0]+" "+product[1]);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute 
+{
+    public long[] minAnd2ndMin(long a[], long n)  
+    {
+        Arrays.sort(a);
+        long arr[] = new long[2];
+        long temp = -1;
+        
+        for(int i = 0; i < a.length-1; i++){
+            if(a[i] < a[i+1]){
+                temp = a[i+1];
+                break;
+            }
+                
+        }
+        if(temp != -1){
+            arr[0] = a[0];
+            arr[1] = temp;
+        }
+        else
+            arr[0] = temp;
+        return arr;
+    }
+}
