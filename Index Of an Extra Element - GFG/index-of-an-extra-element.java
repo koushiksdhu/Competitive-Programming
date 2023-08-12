b81f8c2d14bfccfb451b3b4fcc23a77255be54da
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-08-12 17:57:14 +0530

    Added solution - LeetHub

---
+++ Index Of an Extra Element - GFG/index-of-an-extra-element.java
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+import java.util.*;
+class ExtraElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            int[] b = new int[n - 1];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            for (int i = 0; i < n - 1; i++) b[i] = sc.nextInt();
+            Solution g = new Solution();
+            System.out.println(g.findExtra(a, b, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+class Solution {
+    public int findExtra(int a[], int b[], int n) {
+        for(int i = 0; i < Math.min(a.length, b.length); i++){
+            if(a[i] - b[i] != 0)
+                return i;
+        }
+        return a.length > b.length ? a.length-1 : b.length-1;
+    }
+}
