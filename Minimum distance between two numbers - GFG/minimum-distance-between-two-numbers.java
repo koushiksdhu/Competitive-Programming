36e3b3d1a4f614128c67e3c6785fc50532f62a9d
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-08-12 16:15:59 +0530

    Added solution - LeetHub

---
+++ Minimum distance between two numbers - GFG/minimum-distance-between-two-numbers.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) 
+                a[i] = Integer.parseInt(str[i]);
+            String[] xy = br.readLine().trim().split(" ");
+            int x = Integer.parseInt(xy[0]);
+            int y = Integer.parseInt(xy[1]);
+            Solution g = new Solution();
+            System.out.println(g.minDist(a, n, x, y));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int minDist(int a[], int n, int x, int y) {
+        int first = -1, second = -1;
+        int dis = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < a.length; i++) {
+            if(a[i] == x) {
+                first = i;
+                if(second != -1) {
+                    dis = Math.min(dis, Math.abs(second - first));
+                }
+            } else if(a[i] == y) {
+                second = i;
+                if(first != -1) {
+                    dis = Math.min(dis, Math.abs(second - first));
+                }
+            }
+        }
+        
+        return dis == Integer.MAX_VALUE ? -1 : dis;
+    }
+}
