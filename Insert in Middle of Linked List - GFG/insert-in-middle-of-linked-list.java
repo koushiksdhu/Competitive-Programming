20b432277264aed0427fb67ef891b7e621bd9071
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-08-13 17:39:53 +0530

    Added solution - LeetHub

---
+++ Insert in Middle of Linked List - GFG/insert-in-middle-of-linked-list.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int t = sc.nextInt();
+        
+        while(t-->0){
+            int n = sc.nextInt();   
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++){
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            int key = sc.nextInt();
+            
+            //display(head);
+            Solution obj = new Solution();
+            obj.insertInMid(head, key);
+            display(head);
+            System.out.println();
+        }
+    }
+    
+    public static void display(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data+" ");
+            curr = curr.next;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+Structure of node class is:
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    
+    public Node insertInMid(Node head, int data){
+       int size = 0;
+       Node temp = head;
+       
+        Node n = new Node(data);
+        while(temp != null){
+           size++;
+           temp = temp.next;
+        }
+        
+        temp = head;
+        int mid = (int)Math.ceil((float)size/2);
+        size = 0;
+        
+        while(temp != null){
+            size++;
+            if(size == mid){
+                n.next = temp.next;
+                temp.next = n;
+                break;
+            }
+            temp = temp.next;
+        }        
+      return head;
+    }
+}
