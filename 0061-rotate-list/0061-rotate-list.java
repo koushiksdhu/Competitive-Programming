7d87d21b2169dbf8a70b76661c5663ef70a61571
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-08-20 16:07:21 +0530

    Time: 0 ms (100%), Space: 41.4 MB (63.83%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -14,28 +14,57 @@ class Solution {
         if(head == null || head.next == null)
             return head;
         
-        ListNode tail = head;
-
-        int size = 1;
-        while(tail.next != null){
+        // Brute Force:
+        ListNode temp = head;
+        int size = 0;
+        while(temp != null){
             size++;
-            tail = tail.next;
+            temp = temp.next;
         }
-
         k = k % size;
-        
-        if(k == 0)      // if the value of k and length of the linked list is same then return.
+        if(k == 0)
             return head;
-        
-        ListNode rotateNode = head;
-
-        for(int i = 1; i < size-k; i++){
-            rotateNode = rotateNode.next;
+        for(int i = 1; i <= k; i++){
+            ListNode node = head;
+            while(node.next.next != null){
+                node = node.next;
+            }
+            node.next.next = head;
+            head = node.next;
+            node.next = null;
         }
-
-        tail.next = head;
-        head = rotateNode.next;
-        rotateNode.next = null;
         return head;
+
+
+
+
+
+
+
+        // Optimized Approach:
+
+        // ListNode tail = head;
+
+        // int size = 1;
+        // while(tail.next != null){
+        //     size++;
+        //     tail = tail.next;
+        // }
+
+        // k = k % size;
+        
+        // if(k == 0)      // if the value of k and length of the linked list is same then return.
+        //     return head;
+        
+        // ListNode rotateNode = head;
+
+        // for(int i = 1; i < size-k; i++){
+        //     rotateNode = rotateNode.next;
+        // }
+
+        // tail.next = head;
+        // head = rotateNode.next;
+        // rotateNode.next = null;
+        // return head;
     }
 }
