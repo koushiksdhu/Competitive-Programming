a6becddf13c6ba0247eeb62c5d853f33a628095a
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-08-27 06:24:49 +0530

    Time: 2 ms (86.48%), Space: 40.7 MB (25.78%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
+                stk.push(s.charAt(i));
+            }
+            else if(!stk.isEmpty() && s.charAt(i) == ')' && stk.peek() == '(')
+                stk.pop();
+            else if(!stk.isEmpty() && s.charAt(i) == '}' && stk.peek() == '{')
+                stk.pop();
+            else if(!stk.isEmpty() && s.charAt(i) == ']' && stk.peek() == '[')
+                stk.pop();
+            else
+                return false;
+        }
+        return stk.isEmpty();
+    }
+}
