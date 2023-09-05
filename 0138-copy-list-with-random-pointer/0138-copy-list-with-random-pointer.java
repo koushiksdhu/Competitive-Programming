ed28affac85661c3c4f4b222d9f5c868d7058d49
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-09-05 09:15:15 +0530

    Time: 0 ms (100%), Space: 43.3 MB (50.49%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,21 +15,67 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Node temp =  head;
-        HashMap<Node, Node> hm = new HashMap<>();
+
+        // Brute Force using extra space:
+
+        // Node temp =  head;
+        // HashMap<Node, Node> hm = new HashMap<>();
+        // while(temp != null){
+        //     Node newNode = new Node(temp.val);
+        //     hm.put(temp, newNode);
+        //     temp = temp.next;
+        // }
+
+        // temp = head;
+        // while(temp != null){
+        //     Node n = hm.get(temp);
+        //     n.next = hm.get(temp.next);
+        //     n.random = hm.get(temp.random);
+        //     temp = temp.next;
+        // }
+        // return hm.get(head);
+
+
+
+
+        // Optimized Approach:
+
+        // Step I: Copying Nodes and its values from the original Linked List:
+
+        if(head == null)
+            return head;
+
+        Node temp = head;
         while(temp != null){
             Node newNode = new Node(temp.val);
-            hm.put(temp, newNode);
-            temp = temp.next;
+            newNode.next = temp.next;
+            temp.next = newNode;
+            temp = temp.next.next;
         }
+
+        // Step II: Copying the random pointer link to the new nodes:
 
         temp = head;
         while(temp != null){
-            Node n = hm.get(temp);
-            n.next = hm.get(temp.next);
-            n.random = hm.get(temp.random);
-            temp = temp.next;
+            temp.next.random = temp.random != null ? temp.random.next : null;
+            temp = temp.next.next;
         }
-        return hm.get(head);
+
+        // Step III: Joining back the distorted link of the Original array:
+
+        Node dummy = new Node(-1);
+        Node itr = null;
+        temp = head;
+        dummy.next = temp.next;
+
+        while(temp != null){
+            itr = temp.next;
+            temp.next = itr.next;
+            temp = itr.next;
+            itr.next = temp != null ? temp.next : null;
+        }
+
+        return dummy.next;
+
     }
 }
