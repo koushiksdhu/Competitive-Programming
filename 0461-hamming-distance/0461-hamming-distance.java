ede156a5c618fc257ae3276669cd1bee53921b0a
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-09-08 20:07:25 +0530

    Time: 0 ms (100%), Space: 39.2 MB (68.24%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        while(x > 0 || y > 0){
+            if((x&1) == 1 && (y&1) == 1){
+                count += 0;
+            }
+            else if((x&1) == 1 || (y&1) == 1){
+                count++;
+            }
+            x >>= 1;
+            y >>= 1;
+        }
+        return count;
+    }
+}
