2e33d14762616294fae621941d3dee1884999dfc
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-09-11 18:57:58 +0530

    Time: 9 ms (18.91%), Space: 44.4 MB (59.12%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
@@ -4,24 +4,14 @@ class Solution {
         HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
 
         for(int i = 0; i < groupSizes.length; i++){
-            if(hm.containsKey(groupSizes[i])){
-                // ArrayList<Integer> temp = hm.get(groupSizes[i]);
-                // temp.add(i);
-                // hm.put(groupSizes[i], temp);
+            if(hm.containsKey(groupSizes[i])) 
                 hm.get(groupSizes[i]).add(i);
-                if(hm.get(groupSizes[i]).size() == groupSizes[i]){
-                    al.add(hm.get(groupSizes[i]));
-                    hm.remove(groupSizes[i]);
-                }
-            }
-            else{
-                ArrayList<Integer> temp = new ArrayList<>();
-                temp.add(i);
-                hm.put(groupSizes[i], temp);
-                if(hm.get(groupSizes[i]).size() == groupSizes[i]){
-                    al.add(hm.get(groupSizes[i]));
-                    hm.remove(groupSizes[i]);
-                }
+            else 
+                hm.put(groupSizes[i], new ArrayList<>(Arrays.asList(i)));
+            
+            if(hm.get(groupSizes[i]).size() == groupSizes[i]){
+                al.add(hm.get(groupSizes[i]));
+                hm.remove(groupSizes[i]);
             }
         }
         return al;
