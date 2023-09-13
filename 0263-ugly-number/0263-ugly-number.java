457b6748e4e9c07cb3fa418debbb76a5f59458bc
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-09-13 08:47:05 +0530

    Time: 1 ms (93.67%), Space: 39.3 MB (89.65%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n > 0){
+            for(int i = 2; i < 6; i++){
+                while(n % i == 0)
+                    n /= i;
+            }
+        }
+        return n == 1;
+    }
+}
