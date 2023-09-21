7a0110a40407a7d87b3a10a93cc4fd24feeffd7a
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-09-21 10:59:16 +0530

    Time: 10 ms (47.82%), Space: 40.1 MB (41.63%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    boolean solve(char board[][]){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == '.'){
+                    for(char ch = '1'; ch <= '9'; ch++){
+                        if(isValid(board, i, j, ch)){
+                            board[i][j] = ch;
+
+                            if(solve(board) == true)
+                                return  true;
+                            else 
+                                board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    boolean isValid(char board[][], int row, int col, char c){
+        for(int i = 0; i < 9; i++){
+            if(board[i][col] == c)
+                return false;
+            
+            if(board[row][i] == c)
+                return false;
+            
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == c)
+                return false;
+        }
+        return true;
+    }
+}
