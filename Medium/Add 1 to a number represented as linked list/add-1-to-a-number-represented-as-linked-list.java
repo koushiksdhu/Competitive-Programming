bdfac8d95f154bba471781376109b32425d90269
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-09-22 10:13:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Add 1 to a number represented as linked list/add-1-to-a-number-represented-as-linked-list.java
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    public static Node addOne(Node head) 
+    { 
+        Node temp = reverse(head);
+        Node newHead = temp;
+        int add = 0, carry = 1;
+        while(temp != null){
+            add = temp.data + carry;
+            carry = add / 10;
+            add %= 10;
+            temp.data = add;
+            temp = temp.next;
+        }
+        
+        if(carry > 0){
+            Node carryNode = new Node(carry);
+            carryNode.next = reverse(newHead);
+            temp = carryNode;
+        }
+        else{
+            temp = reverse(newHead);
+        }
+        return temp;
+    }
+    
+    static Node reverse(Node temp){
+        Node prev = null;
+        Node curr = temp;
+        Node next = null;
+        
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
