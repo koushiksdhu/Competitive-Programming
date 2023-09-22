0d889d77590811d6c5c815c07a1c69692d74bcf7
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-09-22 09:23:03 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Rotate a Linked List/rotate-a-linked-list.java
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+} 
+
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            
+            int a = sc.nextInt();
+            Node head = new Node(a);
+            Node tail = head;
+            
+            for (int i=0; i<n-1; i++)
+            {
+                a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution ob = new Solution();
+            head = ob.rotate(head,k);
+            printList(head);
+        }
+    }
+    
+    public static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/* node of linked list:
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+
+*/
+
+class Solution{
+    //Function to rotate a linked list.
+    public Node rotate(Node head, int k) {
+        Node temp = head;
+        Node t = head;
+        int size = 0;
+        while(temp != null && temp.next != null){
+            size++;
+            temp = temp.next;
+        }
+        if(size > 0)
+            size++;
+        
+        k = k % size;
+        if(k == 0)
+            return head;
+            
+        for(int j = 0; j < k-1; j++){
+            t = t.next;
+        }
+        
+        temp.next = head;
+        head = t.next;
+        t.next = null;
+        
+        return head;
+        
+        
+    }
+}
