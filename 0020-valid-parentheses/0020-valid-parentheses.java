813f82406eb0b3c626271364710f648d1cebe959
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-09-23 10:41:16 +0530

    Time: 2 ms (82.71%), Space: 40.3 MB (85.15%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,11 +4,11 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
                 stk.push(s.charAt(i));
-            else if(!stk.isEmpty() && s.charAt(i) == ')' && stk.peek() == '(')
+            else if(s.charAt(i) == ')' && !stk.isEmpty() && stk.peek() == '(')
                 stk.pop();
-            else if(!stk.isEmpty() && s.charAt(i) == '}' && stk.peek() == '{')
+            else if(s.charAt(i) == '}' && !stk.isEmpty() && stk.peek() == '{')
                 stk.pop();
-            else if(!stk.isEmpty() && s.charAt(i) == ']' && stk.peek() == '[')
+            else if(s.charAt(i) == ']' && !stk.isEmpty() && stk.peek() == '[')
                 stk.pop();
             else
                 return false;
