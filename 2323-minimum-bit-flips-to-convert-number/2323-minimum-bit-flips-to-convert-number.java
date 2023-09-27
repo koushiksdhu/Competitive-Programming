34e764b5f2f08f450b36b24abccd810bc2c750bb
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-09-27 08:05:51 +0530

    Time: 0 ms (100%), Space: 39 MB (90.56%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -1,11 +1,20 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        String num1 = String.format("%32s", Integer.toBinaryString(start)).replaceAll(" ", "0");
-        String num2 = String.format("%32s", Integer.toBinaryString(goal)).replaceAll(" ", "0");
+        // String num1 = String.format("%32s", Integer.toBinaryString(start)).replaceAll(" ", "0");
+        // String num2 = String.format("%32s", Integer.toBinaryString(goal)).replaceAll(" ", "0");
+        // int count = 0;
+        // for(int i = 0; i < num1.length(); i++){
+        //     if(num1.charAt(i) != num2.charAt(i))
+        //         count++;
+        // }
+        // return count;
+
         int count = 0;
-        for(int i = 0; i < num1.length(); i++){
-            if(num1.charAt(i) != num2.charAt(i))
+        while(start > 0 || goal > 0){
+            if(((start & 1)^(goal & 1)) == 1)
                 count++;
+            start >>= 1;
+            goal >>= 1;
         }
         return count;
     }
