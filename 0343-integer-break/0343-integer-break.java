aec66d2b5d6895ccfa388451ccb040837cdad605
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-10-06 08:44:11 +0530

    Time: 0 ms (100%), Space: 39.4 MB (28.84%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,8 +1,24 @@
 class Solution {
     public int integerBreak(int n) {
-       if(n <= 3)
-            return n-1;
-        int q = n / 3, rem = n % 3;
-        return rem == 0 ? (int)Math.pow(3, q) : (rem == 1 ? (int)Math.pow(3, q-1) * 4 : (int)Math.pow(3, q) * 2); 
+    //    if(n <= 3)
+    //         return n-1;
+    //     int q = n / 3, rem = n % 3;
+    //     return rem == 0 ? (int)Math.pow(3, q) : (rem == 1 ? (int)Math.pow(3, q-1) * 4 : (int)Math.pow(3, q) * 2); 
+
+    
+
+    // Another Method:
+    if(n == 2)
+        return 1;
+    if(n == 3)
+        return 2;
+
+    int prod = 1;
+    while(n > 4){
+        prod *= 3;
+        n -= 3;
+    }
+    prod *= n;
+    return prod;
     }
 }
