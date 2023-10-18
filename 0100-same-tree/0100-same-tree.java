78b71b55d36b8d453d289f036a995caba9228494
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-10-18 09:11:57 +0530

    Time: 0 ms (100%), Space: 39.2 MB (98.68%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -20,7 +20,7 @@ class Solution {
 
         else if(p == null || q == null || p.val != q.val)
             return false;
-
+        
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
