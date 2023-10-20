c5e03b7c72f557170db03c3a146b8acc928e30ad
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-10-20 10:25:00 +0530

    Time: 0 ms (100%), Space: 40.5 MB (48.92%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -20,12 +20,12 @@ class Solution {
         
         return isMirror(root.left, root.right);
     }
-    boolean isMirror(TreeNode left, TreeNode right){
+    boolean isMirror(TreeNode left, TreeNode right) {
         if(left == null && right == null)
             return true;
-        else if(left == null || right == null)
+        else if(left == null || right == null || left.val != right.val)
             return false;
-
-        return left.val == right.val && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+        
+        return isMirror(left.left, right.right) && isMirror(right.left,left.right);
     }
 }
