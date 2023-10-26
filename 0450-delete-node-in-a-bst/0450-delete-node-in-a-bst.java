240f775065db36bb9338fabefef2642728243e84
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-10-26 21:05:47 +0530

    Time: 0 ms (100%), Space: 44.9 MB (83.33%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -18,49 +18,47 @@ class Solution {
         if(root == null)
             return null;
 
-        if(root.val == key)
-            return helperFunction(root);
+        else if(root.val == key)
+            return helper(root);
 
-        TreeNode temp = root;
-        while(root != null) {
-            if(root.val > key) {
-                if(root.left != null && root.left.val == key) {
-                    root.left = helperFunction(root.left);
+        TreeNode dummy = root;
+        while(dummy != null) {
+            if(dummy.val > key) {
+                if(dummy.left != null && dummy.left.val == key) {
+                    dummy.left = helper(dummy.left);
                     break;
                 }
                 else {
-                    root = root.left;
+                    dummy = dummy.left;
                 }
             }
             else {
-                if(root.right != null && root.right.val == key) {
-                    root.right = helperFunction(root.right);
-                    break;
+                if(dummy.right != null && dummy.right.val == key) {
+                    dummy.right = helper(dummy.right);
+                    break; 
                 }
-                else {
-                    root = root.right;
-                }
+                else
+                    dummy = dummy.right;
             }
         }
-        return temp;
+        return root;
     }
 
-    TreeNode helperFunction(TreeNode root) {
-        if(root.left == null)
-            return root.right;
-        else if(root.right == null)
-            return root.left;
-        else {
-            TreeNode rightChild = root.right;
-            TreeNode lastRight = findLastRight(root.left);
-            lastRight.right = rightChild;
-            return root.left;
-        }
+    TreeNode helper(TreeNode temp) {
+        if(temp.left == null)
+            return temp.right;
+        else if(temp.right == null)
+            return temp.left;
+
+        TreeNode rChild = temp.right;
+        TreeNode lastR = findLastRight(temp.left);
+        lastR.right = rChild;
+        return temp.left;
     }
 
-    TreeNode findLastRight(TreeNode root) {
-        if(root.right == null)
-            return root;
-        return findLastRight(root.right);
+    TreeNode findLastRight(TreeNode temp) {
+        if(temp.right == null)
+            return temp;
+        return findLastRight(temp.right);
     }
 }
