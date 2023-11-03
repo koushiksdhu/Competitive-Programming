54c9782a201dbe50a76be19cae67a23e90904b6e
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-11-03 14:51:22 +0530

    Time: 1 ms (19.26%), Space: 41.3 MB (97.63%) - LeetHub

--- 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.java
@@ -1,17 +1,41 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
+        
+        // METHOD 1:
+        
+        // ArrayList<String> al = new ArrayList<>();
+        // Stack<Integer> stk = new Stack<>();
+        // for(int i = 0, j = 0; i <= n && j < target.length; i++){
+        //     stk.push(i+1);
+        //     al.add("Push");
+        //     if(stk.peek() != target[j]){
+        //         stk.pop();
+        //         al.add("Pop");
+        //     }
+        //     else {
+        //         j++;
+        //     }   
+        // }
+        // return al;
+
+
+
+        // METHOD 2:
+
         ArrayList<String> al = new ArrayList<>();
-        Stack<Integer> stk = new Stack<>();
-        for(int i = 0, j = 0; i <= n && j < target.length; i++){
-            stk.push(i+1);
-            al.add("Push");
-            if(stk.peek() != target[j]){
-                stk.pop();
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i : target)
+            hs.add(i);
+        
+        for(int i = 1; i <= n; i++){
+            if(hs.contains(i))
+                al.add("Push");
+            else {
+                al.add("Push");
                 al.add("Pop");
             }
-            else {
-                j++;
-            }   
+            if(i == target[target.length-1])
+                break;
         }
         return al;
     }
