67ba9c5973f44992fb8dd9af6ae256c875446e2e
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-11-07 08:13:22 +0530

    Time: 2 ms (82.95%), Space: 41.9 MB (15.63%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -0,0 +1,49 @@
+class Pair {
+    int row, col, count;
+    Pair(int row, int col, int count) {
+        this.row = row;
+        this.col = col;
+        this.count = count;
+    }
+}
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        Queue<Pair> q = new  LinkedList<>();
+        boolean vis[][] = new boolean[grid.length][grid[0].length];
+
+        int r[] = {-1, 0, 1, 0};
+        int c[] = {0, 1, 0, -1};
+
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 2){
+                    q.add(new Pair(i, j, 0));
+                    vis[i][j] = true;
+                }
+                else if(grid[i][j] == 0)
+                    vis[i][j] = true;
+            }
+        }
+        int count = 0;
+        while(!q.isEmpty()) {
+            Pair pairData = q.poll();
+            for(int i = 0; i < r.length; i++) {
+                int row = pairData.row + r[i];
+                int col = pairData.col + c[i];
+                count = Math.max(count, pairData.count);
+            if(row >= 0 && row < grid.length && col >= 0 && col < grid[0].length && grid[row][col] == 1 && vis[row][col] == false) {               grid[row][col] = 2;
+                q.add(new Pair(row, col, count+1));
+                vis[row][col] = true;
+                }
+            }
+        }
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(vis[i][j] != true && grid[i][j] == 1)
+                    return -1;
+            }
+        }
+        return count;
+    }
+}
