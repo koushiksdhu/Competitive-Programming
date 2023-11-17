76f60932525559daf82940f1464003cb74f71c88
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-11-17 10:13:28 +0530

    Added Solution - GfG to GitHub

---
+++ School/Find difference between sum of diagonals/find-difference-between-sum-of-diagonals.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            int Grid[][] = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                String[] s = read.readLine().split(" ");
+                for (int j = 0; j < N; j++) Grid[i][j] = Integer.parseInt(s[j]);
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.diagonalSumDifference(N, Grid));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int diagonalSumDifference(int N, int[][] Grid) {
+        
+        int leftToRight = 0, rightToLeft = 0;
+        for(int i = 0; i < Grid.length; i++) {
+            leftToRight += Grid[i][i];
+            rightToLeft += Grid[i][Grid[0].length - 1 - i];
+        }
+        return Math.abs(leftToRight - rightToLeft);
+    }
+}
