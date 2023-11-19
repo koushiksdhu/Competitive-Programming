19953922cf3c4c7099f4b0145a904c0626868755
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2023-11-19 07:10:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Intersection of two sorted Linked lists/intersection-of-two-sorted-linked-lists.java
@@ -0,0 +1,113 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node 
+{
+    int data;
+    Node next;
+    
+    Node(int a)
+        {
+            data = a;
+            next = null;
+        }
+}
+class GfG
+{
+    static Scanner sc = new Scanner(System.in);
+    public static Node inputList(int size)
+    {
+        Node head, tail;
+        int val;
+        
+        val = sc.nextInt();
+        
+        head = tail = new Node(val);
+        
+        size--;
+        
+        while(size-->0)
+        {
+            val = sc.nextInt();
+            tail.next = new Node(val);
+            tail = tail.next;
+        }
+        
+        return head;
+    }
+    
+    public static void printList(Node n)
+    {
+        while(n!=null)
+        {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+    
+    public static void main(String args[])
+        {
+            
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n , m;
+                    
+                    n = sc.nextInt();
+                    m = sc.nextInt();
+                    
+                    Node head1 = inputList(n);
+                    Node head2 = inputList(m);
+                    
+                    Solution obj = new Solution();
+                    
+                    Node result = obj.findIntersection(head1, head2);
+	    
+	                printList(result);
+	                System.out.println();
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution
+{
+   public static Node findIntersection(Node head1, Node head2)
+    {
+        ArrayList<Integer> hs1 = new ArrayList<>();
+        ArrayList<Integer> hs2 = new ArrayList<>();
+        
+        Node temp = head1;
+        while(temp != null){
+            hs1.add(temp.data);
+            temp = temp.next;
+        }
+        
+        temp = head2;
+        while(temp != null) {
+            hs2.add(temp.data);
+            temp = temp.next;
+        }
+        
+        Node ans = new Node(-1);
+        Node h = ans;
+        
+        for(int i : hs1) {
+            if(hs2.contains(i)){
+                h.next = new Node(i);
+                hs2.remove(hs2.indexOf(i));
+                h = h.next;
+            }
+        }
+        return ans.next;
+    }
+}
