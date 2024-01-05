5f8c6b037f17ac5e74b81de500eb9c8b500d51e9
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2024-01-05 17:41:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count possible ways to construct buildings/count-possible-ways-to-construct-buildings.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int N = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int ans = ob.TotalWays(N);
+            System.out.println(ans);           
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int TotalWays(int N)
+    {
+        long mod = (long)1e9+7;
+        if(N == 1)
+            return 4;
+        else if(N == 2)
+            return 9;
+        
+        long prev = 2, curr = 3, ans = 0;
+        for(int i = 3; i <= N; i++) {
+            ans = (prev + curr) % mod;
+            prev = curr;
+            curr = ans;
+        }
+        return (int)((ans * ans) % mod);
+    }
+}
