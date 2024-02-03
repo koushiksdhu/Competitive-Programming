ee4d74c930a1fb3b2208fb5ec8f4deb8ec353844
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2024-02-03 10:57:46 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Decimal Equivalent of Binary Linked List/decimal-equivalent-of-binary-linked-list.java
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+class LinkedList1
+{
+    Node head;  // head of lisl
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node)
+    {
+        if (head == null)
+        {
+            head = node;
+        }
+        else
+        {
+            Node temp = head;
+            while (temp.next != null)
+                temp = temp.next;
+
+            temp.next = node;
+        }
+    }
+    /* Function to print linked list */
+    void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        String s = sc.nextLine();
+        while(t>0)
+        {
+            int n = sc.nextInt();
+            String s1 = sc.nextLine();
+            LinkedList1 llist = new LinkedList1();
+
+            if (n > 0)
+            {
+                int a1=sc.nextInt();
+                Node head= new Node(a1);
+                llist.addToTheLast(head);
+            }
+            for (int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+            System.out.println(new Solution().DecimalValue(llist.head));
+            t--;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+ Linked List class
+class LinkedList
+{
+    Node head;  // head of list
+}
+This is method only submission.  You only need to complete the method. */
+
+class Solution
+{
+   long DecimalValue(Node head)
+   {
+        if(head == null)
+            return 0;
+        long MOD = (long)(1e9+7);
+ 	    ArrayList<Integer> al = new ArrayList<>();
+ 	    Node tempNode = head;
+ 	    while(tempNode != null) {
+ 	        al.add(tempNode.data);
+ 	        tempNode = tempNode.next;
+ 	    }
+ 	    
+ 	    int exp = al.size() - 1;
+ 	    long sum = 0;
+ 	    
+ 	    for(int i : al) {
+ 	         if (i == 1)
+                sum = (sum * 2 + 1) % 1000000007;  
+            else
+                sum = (sum * 2) % 1000000007;  
+ 	    }
+ 	        
+ 	    return sum % 1000000007;
+   }
+}
