552e55260146474fbf869ea447f478c670257ade
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2024-02-05 12:24:15 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Second Largest/second-largest.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.stream.Collectors; 
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().print2largest(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int print2largest(int arr[], int n) {
+        int max1 = -1, max2 = -1;
+        for(int i : arr)
+            max1 = Math.max(max1, i);
+            
+        for(int i : arr)
+            if(i < max1) {
+                max2 = Math.max(max2, i);
+            }
+        return max2;
+    }
+}
