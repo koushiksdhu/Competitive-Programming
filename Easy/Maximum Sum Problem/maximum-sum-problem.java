be4e50fe440605f53d88c7789201f3d28f3db5df
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2024-02-24 12:03:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximum Sum Problem/maximum-sum-problem.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.maxSum(n));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int maxSum(int n) 
+    { 
+        if(n == 0 || n == 1)
+            return n;
+            
+        int ans = (int)(maxSum(n / 2) + maxSum(n / 3) + maxSum(n / 4));
+        return Math.max(ans, n);
+    } 
+}
