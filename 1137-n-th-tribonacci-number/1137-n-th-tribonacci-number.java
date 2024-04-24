0a9a7d448da34ff561f809d8d27d698dbd6981d1
Koushik Sadhu <76591181+koushiksdhu@users.noreply.github.com>
2024-04-24 20:52:58 +0530

    Time: 0 ms (100%), Space: 40.2 MB (59.37%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,9 +1,7 @@
 class Solution {
     public int tribonacci(int n) {
-        if(n <= 1)
+        if(n < 2)
             return n;
-        else if(n == 2)
-            return 1;
         int a = 0, b = 1, c = 1;
         int sum = 0;
         for(int i = 3; i <= n; i++) {
@@ -12,6 +10,6 @@ class Solution {
             b = c;
             c = sum;
         }
-        return sum;
+        return c;
     }
 }
